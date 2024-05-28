c79be133434ce6553e47fca1cdf2dc87a595006e
EddieProgammer24 <eddielundu55@gmail.com>
2024-05-28 20:01:53 +0300

    Admin Forms

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -3,12 +3,50 @@
     <!-------- Main Content Section Starts ----->
     <div class="main-content">
     <div class="wrapper">
-        <h1>Manage Admin</h1>
+        <h1>Manage Admin</h1><br/><br>
 
-       
+        <!----- Button to add admin ------>
+        <a href="add-admin.php" class="btn-primary">Add Admin</a><br/><br/><br/>
 
-        
+        <table class="tbl-full">
+            <tr>
+                <th>S.N.</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
 
+            <tr>
+                <td>1.</td>
+                <td>Vijay Thapa</td>
+                <td>vijaythapa</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                    
+                </td>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Vijay Thapa</td>
+                <td>vijaythapa</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Vijay Thapa</td>
+                <td>vijaythapa</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
         
 
     </div>
