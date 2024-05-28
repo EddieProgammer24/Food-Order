7cc6ae43b2434d0328e868300037444c56973652
EddieProgammer24 <eddielundu55@gmail.com>
2024-05-28 13:25:51 +0300

    Set Up Admin Files

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+ <!-------- Footer Section Starts ----->
+ <div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2024 All Rights Reserved. Developed By <a href="https://github.com/EddieProgammer24">- Eddie Lundu</a></p>
+        </div>
+        
+</div>
+    <!-------- Footer Section Ends ---->
+</body>
+</html>
