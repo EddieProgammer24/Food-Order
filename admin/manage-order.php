38d6db3113c6dd3b37b70d28b60744de7347f0f5
EddieProgammer24 <eddielundu55@gmail.com>
2024-06-05 14:33:36 +0300

    Food Category

--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,44 +4,89 @@
     <div class="wrapper">
     <h1>Manage Order</h1><br/><br/><br/>
 
+<?php
+    if(isset($_SESSION['update']))
+    {
+        echo $_SESSION['update'];
+        unset($_SESSION['update']);
+    }
+?>
+<br><br>
 <table class="tbl-full">
     <tr>
         <th>S.N.</th>
-        <th>Full Name</th>
-        <th>Username</th>
+        <th>Food</th>
+        <th>Price</th>
+        <th>Qty.</th>
+        <th>Total</th>
+        <th>Order Date</th>
+        <th>Status</th>
+        <th>Customer Name</th>
+        <th>Customer Contact</th>
+        <th>Email</th>
+        <th>Adrress</th>
         <th>Actions</th>
     </tr>
 
-    <tr>
-        <td>1.</td>
-        <td>Vijay Thapa</td>
-        <td>vijaythapa</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
+    <?php
+    //get all orders from database
+    $sql = "SELECT * FROM tbl_order ORDER BY id DESC";
+    //Execute the query
+    $res = mysqli_query($conn,$sql);
+    //count the rows
+    $count = mysqli_num_rows($res);
+
+    $sn = 1;
+
+    if($count>0)
+    {
+        //order available
+        while($row=mysqli_fetch_assoc($res))
+        {
+            //get all the order details
+            $id = $row['id'];
+            $food = $row['food'];
+            $price = $row['price'];
+            $qty = $row['qty'];
+            $total = $row['total'];
+            $order_date = $row['order_date'];
+            $status = $row['status'];
+            $customer_name = $row['customer_name'];
+            $customer_contact = $row['customer_contact'];
+            $customer_email = $row['customer_email'];
+            $customer_address = $row['customer_address'];
+
+            ?>
+                    <tr>
+                         <td><?php echo $sn++;?>.</td>
+                         <td><?php echo $food;?></td>
+                         <td><?php echo $price;?></td>
+                         <td><?php echo $qty;?></td>
+                         <td><?php echo $total;?></td>
+                         <td><?php echo $order_date;?></td>
+                         <td><?php echo $status;?></td>
+                         <td><?php echo $customer_name;?></td>
+                         <td><?php echo $customer_contact;?></td>
+                         <td><?php echo $customer_email;?></td>
+                         <td><?php echo $customer_address;?></td>
+                    <td>
+                        <a href="<?php echo SITEURL;?>/admin/update-order.php?id=<?php echo $id;?>" class="btn-secondary">Update Order</a>
             
-        </td>
-    </tr>
+                     </td>
+                  </tr>
 
-    <tr>
-        <td>1.</td>
-        <td>Vijay Thapa</td>
-        <td>vijaythapa</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+            <?php
+        }
+    }
+    else
+    {
+        //order not available
+        echo "<tr><td colspan='12' class='error'>Order not Available</td></tr>";
+    }
+    ?>
 
-    <tr>
-        <td>1.</td>
-        <td>Vijay Thapa</td>
-        <td>vijaythapa</td>
-        <td>
-            <a href="#" class="btn-secondary">Update Admin</a>
-            <a href="#" class="btn-danger">Delete Admin</a>
-        </td>
-    </tr>
+    
+   
 </table>
 
     </div>
